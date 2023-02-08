6764b52501cdc57888598a6854a5d9603a16f74b
jeevan kumar <jjjeeevvvaaan@gmail.com>
2023-02-08 14:14:15 +0530

    Added images.

--- src/components/Header/Trophies.js
+++ src/components/Header/Trophies.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Trophies = ({ data: { character }}) =>
 	<div className="trophy">
 		<img
-			src="/images/trophy2.jpg"
+			src="/images/trophy2.png"
 			alt="coin"
 			height="20%"
 			width="20%"
